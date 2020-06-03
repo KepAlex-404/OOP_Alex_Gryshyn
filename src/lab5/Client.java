c4b7d3c746cef20c972660bebfb4e81275720dae
KepAlex-404 <54204040+KepAlex-404@users.noreply.github.com>
2020-06-03 13:21:00 +0300

    new lab5

--- src/lab5/Client.java
+++ src/lab5/Client.java
@@ -8,7 +8,7 @@ public class Client {
     }
 
     public void startLiveInTheSystem() {
-        System.out.println("Word entrances quantity: " + text.findWordEntrancesQuantity("Hello"));
+        System.out.println("Word entrances quantity: " + Text.task(text.toString(),'a'));
     }
 
 }
